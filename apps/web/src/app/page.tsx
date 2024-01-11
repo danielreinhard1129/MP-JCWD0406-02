51b7e6f06990691123e6b1aaca1f6e91bbdc6e92
suyanto98 <suyantolearn09@gmail.com>
2024-01-11 22:00:22 +0700

    chore: fix bug flowbite

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,9 +1,12 @@
-'use client';
+
+import { Button, Card, Dropdown, DropdownItem } from "flowbite-react";
 
 export default function Home() {
   return (
-    <div>
-      <div>hello</div>
-    </div>
+    <main className="container max-w-7xl px-4 mx-auto">
+      <section className="mt-20 flex flex-col items-center justify-center">
+        <h1 className="mb-4 text-4xl font-semibold mt-120">Home Page</h1>
+      </section>
+    </main>
   );
 }
